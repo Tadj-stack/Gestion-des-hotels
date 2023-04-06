5943ecd7995db26a282b5311d001be9bf61821f3
Tadj <eddinetadj@gmail.com>
2023-04-06 00:43:19 -0400

    la classe client est cree

--- Gestion-des-hotels/Gestion-des-hotels/Client.cs
+++ Gestion-des-hotels/Gestion-des-hotels/Client.cs
@@ -8,5 +8,28 @@ namespace Gestion_des_hotels
 {
     internal class Client
     {
+
+        //creation des propriete de la classe client sans attributs parceque il n y a pas des contraints sur les attributs  
+
+        
+
+
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+        public string NbCarte { get; set; }
+        public string NbChambre { get; set; }
+
+
+        //creation d'un constructeur qui intialise tout les atributs 
+        public Client(string nom, string prenom, string email, string nbCarte, string nbChambre)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Email = email;
+            NbCarte = nbCarte;
+            NbChambre = nbChambre;
+        }
+
     }
 }
