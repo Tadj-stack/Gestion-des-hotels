62e12a00645bb269d016bcd8df9e45f6c3ce8807
Tadj <eddinetadj@gmail.com>
2023-04-06 00:31:57 -0400

    la creation de classe client et hotel

---
+++ Gestion-des-hotels/Gestion-des-hotels/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestion_des_hotels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
