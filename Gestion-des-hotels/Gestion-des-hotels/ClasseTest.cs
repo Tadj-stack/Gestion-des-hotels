3718fab649041e98abf77ece64bbba1152bad6ac
Tadj <eddinetadj@gmail.com>
2023-04-13 16:30:39 -0400

    la version finale apres le test

--- Gestion-des-hotels/Gestion-des-hotels/ClasseTest.cs
+++ Gestion-des-hotels/Gestion-des-hotels/ClasseTest.cs
@@ -19,13 +19,32 @@
             Client client12= new Client("Davis", "Michael", "michael.davis@gmail.com", "7777 8888 9999 0000", 46);
             Client client13= new Client("Wilson", "Sarah", "sarah.wilson@hotmail.com", "6666 7777 8888 9999", 39);
             Client client14= new Client("Brown", "Emma", "emma.brown@gmail.com", "1234 5678 9012 3456", 41);
-            Client client15 = new Client("mohammed", "assas", "moh.assas@gmail.com", "1234 5678 9012 3456", 2);
+            Client client15 = new Client("mohammed", "assas", "moh.assas@gmail.com", "1234 5678 9012 3456", 14);
+            Client client16 = new Client("tadj", "eddine", "edd.tell@gmail.com", "1234 5678 9012 3456", 100);
 
+           //Liste des clients 
+            
             List<Client> CLients = new List<Client>{ client1 , client2, client3, client4 , client5, client6 , client7, client8, client9 ,client10, client11, client12
                 ,client12,client13,client14,client15};
+            
 
+            //creation de l hotel
             Hotel hotel = new Hotel("Arc En Ciel", CLients);
+
+            
+            hotel.AjouterClient(client16);
+
+
+            hotel.AjouterClient(client10);
+
+            
+            hotel.SuppurimerClient(client12);
+
+         
+            //affichage de le hotel "test toString"
             Console.WriteLine(hotel);
+
+            //l'affichage des clients de l hotel
             foreach(Client client in CLients)
             {
                 Console.WriteLine(client);
--- Gestion-des-hotels/Gestion-des-hotels/Hotel.cs
+++ Gestion-des-hotels/Gestion-des-hotels/Hotel.cs
@@ -22,35 +22,34 @@
         //Methode pour ajouter un client a la liste des client de l'hotel 
         public void AjouterClient(Client client)
         {
-            foreach (Client clnt1 in Clients)
-            {
 
-                //une condition pour verifier si le client se trouve deja dans la liste des clients 
-                if (!clnt1.Equals(client))
-                {
-                    Clients.Add(client);
-                }
-                return;
+
+
+            //une condition pour verifier si le client se trouve deja dans la liste des clients 
+            if (!Clients.Contains(client))
+            {
+                
+                Clients.Add(client);
             }
+            else { Console.WriteLine($"Le client {client.Nom} {client.Prenom} existe deja dans la liste des clients"); }
+
         }
         //Methode permet de supprimer un client a la liste des client de l'hotel
         public void SuppurimerClient(Client client)
         {
-            foreach (Client clnt1 in Clients)
+
+            //une condition pour verifier si le client se trouve deja dans la liste des clients 
+            if (Clients.Contains(client))
+
             {
-
-                //une condition pour verifier si le client se trouve deja dans la liste des clients 
-                if (!clnt1.Equals(client))
-                {
-                    Clients.Remove(client);
-                }
-                return;
+                Clients.Remove(client);
             }
-
-
-
-
-
+            else { Console.WriteLine($"Le client {client.Nom} {client.Prenom} n'existe pas dans la liste des clients"); }
         }
+
+
+
+
+
     }
 }
